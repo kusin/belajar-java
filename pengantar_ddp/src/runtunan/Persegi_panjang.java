9d7e34fb93217d5051f42f8e7305dee4ca09d255
kusin <aryajayaalamsyah@gmail.com>
2022-12-31 01:32:24 +0700

    -

---
+++ pengantar_ddp/src/runtunan/Persegi_panjang.java
@@ -0,0 +1,23 @@
+// deklarasi package
+package runtunan;
+
+// deklarasi pustaka
+import java.util.Scanner;
+
+// main class
+public class Persegi_panjang {
+    
+    //main method
+    public static void main(String[]args){
+        
+        // deklarasi obj scanner
+        Scanner obj = new Scanner(System.in);
+        
+        // deklarasi variabel
+        double panjang, lebar;
+        
+        // masukan program
+        System.out.print("Masukan panjang : ");
+        panjang = obj.nextDouble();
+    }
+}
